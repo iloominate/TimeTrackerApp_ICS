f200ab40af69089ca8c17bb0b5aa64aad4ae900e
Maxim Kalutsky <gmamyako1@outlook.com>
2023-02-27 00:37:39 +0100

    Add project files.

---
+++ TimeTracker.DAL.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TimeTracker.DAL.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TimeTracker.DAL/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace TimeTracker.DAL
+{
+    public class Class1
+    {
+
+    }
+}
