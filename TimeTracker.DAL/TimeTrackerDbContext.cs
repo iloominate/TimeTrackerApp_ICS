39b74b99065cdc59a59dc0314e2a8a18c3a4ed33
Maxim Kalutsky <gmamyako1@outlook.com>
2023-02-28 12:03:53 +0100

    (feat) adds DbContext

---
+++ TimeTracker.DAL/TimeTrackerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TimeTracker.DAL
+{
+    public class TimeTrackerDbContext : DbContext
+    {
+        public DbSet<Entities.ActivityEntity> Activities => Set<Entities.ActivityEntity>();
+        public DbSet<Entities.ProjectEntity> Projects => Set<Entities.ProjectEntity>();
+        public DbSet<Entities.UserEntity> Users => Set<Entities.UserEntity>();
+    }
+}
