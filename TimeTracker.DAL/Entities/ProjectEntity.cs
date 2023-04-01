44f65d3ca5d6c5266d54bed48235064919b781e8
SyruSTR <dargonoed25@mail.ru>
2023-04-01 14:53:25 +0200

    (refactor) Fix name

--- TimeTracker.DAL/Entities/ProjectEntity.cs
+++ TimeTracker.DAL/Entities/ProjectEntity.cs
@@ -9,7 +9,7 @@ public record ProjectEntity : IEntity
     
     public ICollection<ProjectAmountEntity> Users { get; set; } = new List<ProjectAmountEntity>();
 
-    public ICollection<ActivityEntity> activities { get; set; } = new List<ActivityEntity>();
+    public ICollection<ActivityEntity> Activities { get; set; } = new List<ActivityEntity>();
 
     public required Guid CreatorId { get; set; }
     public required UserEntity Creator { get; set; }
