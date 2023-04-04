cee67d7645709e806afbdf86a148c98f3dddecf3
Maxim Kalutsky <gmamyako1@outlook.com>
2023-04-04 11:37:34 +0200

    (feat) add dto connections for activities and seed data

--- TimeTracker.DAL/TimeTrackerDbContext.cs
+++ TimeTracker.DAL/TimeTrackerDbContext.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TimeTracker.DAL.Entities;
-using Microsoft.EntityFrameworkCore;
 using TimeTracker.DAL.Seeds;
+
+using TimeTracker.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace TimeTracker.DAL
 {
-    
-
     public class TimeTrackerDbContext : DbContext
     {
         private readonly bool _seedDemoData;
@@ -23,24 +23,44 @@ namespace TimeTracker.DAL
         public DbSet<ProjectEntity> Projects => Set<ProjectEntity>();
         public DbSet<UserEntity> Users => Set<UserEntity>();
         public DbSet<ProjectAmountEntity>  ProjectAmounts => Set<ProjectAmountEntity>();
-
         
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            //todo relationships options
-
-            if (_seedDemoData)
-            {
-                UserSeeds.Seed(modelBuilder);
-                ProjectSeeds.Seed(modelBuilder);
-                ProjectAmountSeeds.Seed(modelBuilder);
-                ActivitySeeds.Seed(modelBuilder);
-            }
+            modelBuilder.Entity<UserEntity>()
+                .HasMany(i => i.Projects)
+                .WithOne(i => i.User)
+                .OnDelete(DeleteBehavior.Cascade);
+            
+            modelBuilder.Entity<ProjectEntity>()
+                .HasMany<ProjectAmountEntity>()
+                .WithOne(i => i.Project)
+                .OnDelete(DeleteBehavior.Cascade);
+            
+            modelBuilder.Entity<ProjectEntity>()
+                .HasMany<ProjectAmountEntity>()
+                .WithOne(i => i.Project)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            modelBuilder.Entity<ActivityEntity>()
+                .HasOne(i => i.Project)
+                .WithMany(i => i.Activities)
+                .HasForeignKey(i => i.ProjectId)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            modelBuilder.Entity<ActivityEntity>()
+                .HasOne(i => i.User)
+                .WithMany(i => i.Activities)
+                .HasForeignKey(i => i.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+                
+            if (!_seedDemoData) return;
+            
+            UserSeeds.Seed(modelBuilder);   
+            ProjectSeeds.Seed(modelBuilder);
+            ProjectAmountSeeds.Seed(modelBuilder);
+            ActivitySeeds.Seed(modelBuilder);
         }
-
-
     }
 }
