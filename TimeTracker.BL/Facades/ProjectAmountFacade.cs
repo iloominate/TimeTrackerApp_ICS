e8d8eb0e7d26c2e780a3c08fdcb3574ad820ca03
Aliaksei <xklima34@vutbr.cz>
2023-04-08 22:18:31 +0200

    Facades with basic properties added. TODO: Find a way to update collections in Entities. Modify detail and list models if needed

---
+++ TimeTracker.BL/Facades/ProjectAmountFacade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.BL.Facades.Interfaces;
+using TimeTracker.BL.Mappers;
+using TimeTracker.BL.Models.DetailModels;
+using TimeTracker.BL.Models.ListModels;
+using TimeTracker.DAL.Entities;
+using TimeTracker.DAL.Mappers;
+using TimeTracker.DAL.UnitOfWork;
+
+namespace TimeTracker.BL.Facades;
+
+public class ProjectAmountFacade : FacadeBase<ProjectAmountEntity, ProjectAmountListModel, 
+    ProjectAmountDetailModel, ProjectAmountEntityMapper>, IProjectAmountFacade
+{
+    private readonly IProjectAmountModelMapper _projectAmountModelMapper;
+    public ProjectAmountFacade(
+        IUnitOfWorkFactory unitOfWorkFactory,
+        IProjectAmountModelMapper projectAmountModelMapper)
+        : base(unitOfWorkFactory, projectAmountModelMapper) =>
+        _projectAmountModelMapper = projectAmountModelMapper;
+
+}
---
+++ TimeTracker.BL/Facades/ProjectFacade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.BL.Facades.Interfaces;
+using TimeTracker.BL.Mappers;
+using TimeTracker.BL.Models.DetailModels;
+using TimeTracker.BL.Models.ListModels;
+using TimeTracker.DAL.Entities;
+using TimeTracker.DAL.Mappers;
+using TimeTracker.DAL.UnitOfWork;
+
+namespace TimeTracker.BL.Facades;
+
+public class ProjectFacade : FacadeBase<ProjectEntity, ProjectListModel,
+    ProjectDetailModel, ProjectEntityMapper>, IProjectFacade
+{
+    private readonly IProjectModelMapper _projectModelMapper;
+    public ProjectFacade(
+        IUnitOfWorkFactory unitOfWorkFactory,
+        IProjectModelMapper projectModelMapper
+        ) : base(unitOfWorkFactory, projectModelMapper) =>
+        _projectModelMapper = projectModelMapper;
+}
