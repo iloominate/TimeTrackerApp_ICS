8e3e4557c0a504dffefeabc97720f23fafe16174
Aliaksei <xklima34@vutbr.cz>
2023-04-08 15:49:22 +0200

    Mappers to Models created. Only basic functions added

---
+++ TimeTracker.BL/Mappers/ActivityModelMapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.BL.Models;
+using TimeTracker.BL.Models.DetailModels;
+using TimeTracker.BL.Models.ListModels;
+using TimeTracker.DAL.Entities;
+
+namespace TimeTracker.BL.Mappers;
+
+public class ActivityModelMapper : ModelMapperBase<ActivityEntity, ActivityListModel, ActivityDetailModel>,
+    IActivityModelMapper
+{
+
+    public override ActivityListModel MapToListModel(ActivityEntity? entity)
+        => entity is null
+            ? ActivityListModel.Empty
+            : new ActivityListModel { 
+                Id = entity.Id,
+                Start = entity.Start,
+                End = entity.End,
+                Type = entity.Type,
+                UserId = entity.UserId,
+                ProjectId = entity.ProjectId
+            };
+
+    public override ActivityDetailModel MapToDetailModel(ActivityEntity? entity)
+        => entity is null
+            ? ActivityDetailModel.Empty
+            : new ActivityDetailModel
+            {
+                Id = entity.Id,
+                Start = entity.Start,
+                End = entity.End,
+                Type = entity.Type,
+                UserId = entity.UserId,
+                ProjectId = entity.ProjectId,
+            };
+
+    public override ActivityEntity MapToEntity(ActivityDetailModel model)
+        => new() 
+        {
+            Id = model.Id, 
+            Start = model.Start,
+            End = model.End,
+            Type = model.Type,
+            UserId = model.UserId,
+            ProjectId = model.ProjectId
+        };
+
+
+}
+    
---
+++ TimeTracker.BL/Mappers/UserModelMapper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.BL.Models.DetailModels;
+using TimeTracker.BL.Models.ListModels;
+using TimeTracker.DAL.Entities;
+
+namespace TimeTracker.BL.Mappers;
+
+public class UserModelMapper : ModelMapperBase<UserEntity, UserListModel, UserDetailModel>
+{
+    private readonly IProjectAmountModelMapper _projectAmountModelMapper;
+    private readonly IProjectModelMapper _projectModelMapper;
+    private readonly IActivityModelMapper _activityModelMapper;
+
+    public override UserListModel MapToListModel(UserEntity? entity)
+        => entity is null
+            ? UserListModel.Empty
+            : new UserListModel
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Surname = entity.Surname,
+            };
+
+    public override UserDetailModel MapToDetailModel (UserEntity? entity)
+        => entity is null
+            ? UserDetailModel.Empty
+            : new UserDetailModel
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Surname = entity.Surname,
+                PhotoUrl = entity.PhotoUrl,
+                Activities = _activityModelMapper.MapToListModel(entity.Activities)
+                    .ToObservableCollection(),
+                Projects = _projectAmountModelMapper.MapToListModel(entity.Projects)
+                    .ToObservableCollection(),
+                ProjectsCreared = _projectModelMapper.MapToListModel(entity.CreatedProjects)
+                    .ToObservableCollection()
+            };
+
+    public override UserEntity MapToEntity(UserDetailModel model)
+        => new()
+        {
+            Id = model.Id,
+            Name = model.Name,
+            Surname = model.Surname,
+            PhotoUrl = model.PhotoUrl
+        };
+    
+
+}
