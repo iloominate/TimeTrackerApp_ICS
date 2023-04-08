b13784e2d2ff97b5b24b7d20c93eb3bf6a8ca716
Aliaksei <xklima34@vutbr.cz>
2023-04-08 16:01:54 +0200

    Facades interfaces added

---
+++ TimeTracker.BL/Facades/Interfaces/IActivityFacade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.BL.Models.DetailModels;
+using TimeTracker.BL.Models.ListModels;
+using TimeTracker.DAL.Entities;
+
+namespace TimeTracker.BL.Facades.Interfaces;
+
+public interface IActivityFacade : IFacade<ActivityEntity, ActivityListModel, ActivityDetailModel>
+{
+}
---
+++ TimeTracker.BL/Facades/Interfaces/IFacade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.DAL.Entities;
+using TimeTracker.BL.Models;
+
+namespace TimeTracker.BL.Facades.Interfaces;
+
+public interface IFacade<TEntity, TListModel, TDetailModel>
+    where TEntity : class, IEntity
+    where TListModel : IModel
+    where TDetailModel : class, IModel
+{
+    Task DeleteAsync(Guid id);
+    Task<TDetailModel?> GetAsync(Guid id);
+    Task<IEnumerable<TListModel>> GetAsync();
+    Task<TDetailModel> SaveAsync(TDetailModel model);
+}
