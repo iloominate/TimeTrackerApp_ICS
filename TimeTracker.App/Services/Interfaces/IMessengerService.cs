3244f44eb3a6d92acfc0c824f28971952b4419f8
Aliaksei <xklima34@vutbr.cz>
2023-05-01 17:09:43 +0200

    IViewModel, ViewModelBase created. MessengerService created. Folders for ViewModels added

---
+++ TimeTracker.App/Services/Interfaces/IMessengerService.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker.App.Services;
+
+public interface IMessengerService
+{
+    IMessenger Messenger { get; }
+
+    void Send<TMessage>(TMessage message)
+        where TMessage : class;
+}
