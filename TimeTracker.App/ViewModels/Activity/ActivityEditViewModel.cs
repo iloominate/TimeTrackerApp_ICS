dde5e49d5df2d30a2f19ddaa2c63dcfd3531eb5d
Aliaksei <xklima34@vutbr.cz>
2023-05-03 13:55:44 +0200

    ActivityEdit ViewModel finished

--- TimeTracker.App/ViewModels/Activity/ActivityEditViewModel.cs
+++ TimeTracker.App/ViewModels/Activity/ActivityEditViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeTracker.App.Messages;
+using TimeTracker.App.Services;
 using TimeTracker.App.Services.Interfaces;
 using TimeTracker.BL.Facades.Interfaces;
 using TimeTracker.BL.Models.DetailModels;
@@ -16,5 +19,25 @@ public partial class ActivityEditViewModel : ViewModelBase
 
     public ActivityDetailModel Activity { get; init; } = ActivityDetailModel.Empty;
 
+    public ActivityEditViewModel(
+        IActivityFacade activityFacade,
+        INavigationService navigationService,
+        IMessengerService messengerService)
+        : base(messengerService)
+    {
+        _activityFacade = activityFacade;
+        _navigationService = navigationService;
+    }
+
+    [RelayCommand]
+
+    private async Task SaveAsync()
+    {
+        await _activityFacade.SaveAsync(Activity);
+        MessengerService.Send(new ActivityEditMessage { ActivityId = Activity.Id });
+
+        _navigationService.SendBackButtonPressed();
+    }
+   
     
 }
