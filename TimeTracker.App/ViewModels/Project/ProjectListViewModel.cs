ffe9bf3e6837a4ee41e552ad6104e7b7132ef523
Aliaksei <xklima34@vutbr.cz>
2023-05-13 15:08:44 +0200

    Join and Leave functions fixed.

--- TimeTracker.App/ViewModels/Project/ProjectListViewModel.cs
+++ TimeTracker.App/ViewModels/Project/ProjectListViewModel.cs
@@ -20,12 +20,13 @@ namespace TimeTracker.App.ViewModels.Project;
 public partial class ProjectListViewModel : ViewModelBase, 
     IRecipient<ProjectEditMessage>,
     IRecipient<ProjectDeleteMessage>,
-    IRecipient<ProjectToUserAdd>
+    IRecipient<UserToProjectAdd>
 {
     private readonly IProjectFacade _projectFacade;
     private readonly IProjectAmountFacade _projectAmountFacade;
     private readonly IProjectAmountModelMapper _projectAmountModelMapper;
     private readonly IUserFacade _userFacade;
+    private readonly IAlertService _alertService;
     private readonly INavigationService _navigationService;
 
     public IEnumerable<ProjectListModel> Projects { get; set; } = null!;
@@ -38,7 +39,8 @@ public partial class ProjectListViewModel : ViewModelBase,
         IProjectAmountModelMapper projectAmountModelMapper,
         IUserFacade userFacade, 
         INavigationService navigationService,
-        IMessengerService messengerService)
+        IMessengerService messengerService,
+        IAlertService alertService)
         : base(messengerService)
     {
         _projectFacade = projectFacade;
@@ -46,6 +48,7 @@ public partial class ProjectListViewModel : ViewModelBase,
         _userFacade = userFacade;
         _projectAmountModelMapper = projectAmountModelMapper;
         _navigationService = navigationService;
+        _alertService = alertService;
     }
 
     protected override async Task LoadDataAsync()
@@ -56,9 +59,9 @@ public partial class ProjectListViewModel : ViewModelBase,
     }
 
     [RelayCommand]
-    private async Task GoToDetailAsync(Guid id)
-        => await _navigationService.GoToAsync<ProjectDetailViewModel>(
-            new Dictionary<string, object?> { [nameof(ProjectDetailViewModel.Id)] = id });
+    private async Task GoToEditAsync(Guid id)
+        => await _navigationService.GoToAsync<ProjectEditViewModel>(
+            new Dictionary<string, object?> { [nameof(ProjectEditViewModel.projectId)] = id });
 
     [RelayCommand]
     private async Task GoToCreateAsync()
@@ -69,40 +72,77 @@ public partial class ProjectListViewModel : ViewModelBase,
     [RelayCommand]
     private async Task JoinAsync(ProjectListModel projectListModel)
     {
-        if ( ActiveUserId != Guid.Empty && 
+        if (ActiveUserId != Guid.Empty &&
             projectListModel != null
             )
         {
             ProjectAmountDetailModel projectAmountDetailModelNew = _projectAmountModelMapper.MapToNewDetailModel(projectListModel, ActiveUserId);
-            await _projectAmountFacade.SaveAsync(projectAmountDetailModelNew);
 
-            UserDetailModel activeUser = await _userFacade.GetAsync (ActiveUserId);
+            UserDetailModel activeUser = await _userFacade.GetAsync(ActiveUserId);
 
             if (activeUser == null)
             {
                 throw new NullReferenceException("An error occured trying to join project.");
             }
 
-            activeUser.Projects.Add(_projectAmountModelMapper.MapToListModel(projectAmountDetailModelNew));
-
-            ProjectDetailModel projectToJoin = await _projectFacade.GetAsync(projectListModel.Id);
-            
-            if (projectToJoin == null ) 
-            { 
-                throw new NullReferenceException("An error occurred trying to add user to project.");
+            if (activeUser.Projects.Any(p => p.ProjectId == projectListModel.Id && p.UserId == ActiveUserId))
+            {
+                await _alertService.DisplayAsync("JoinUserIsInProject", "User is already in project"); // fix alerts 
             }
+            else
+            {
+                await _projectAmountFacade.SaveAsync(projectAmountDetailModelNew);
 
-            projectToJoin.Users.Add(_projectAmountModelMapper.MapToListModel(projectAmountDetailModelNew));
+                ProjectDetailModel projectToJoin = await _projectFacade.GetAsync(projectListModel.Id);
 
-            MessengerService.Send(new ProjectToUserAdd());
-            MessengerService.Send(new UserToProjectAdd());
+                if (projectToJoin == null)
+                {
+                    throw new NullReferenceException("An error occurred trying to add user to project.");
+                }
+
+                MessengerService.Send(new UserToProjectAdd());
+            }
         }
     }
 
-    public async void Receive(ProjectToUserAdd message)
+    [RelayCommand]
+    public async void LeaveAsync(ProjectListModel projectListModel)
     {
+        if (ActiveUserId != Guid.Empty &&
+            projectListModel != null
+            )
+        {
+
+            UserDetailModel activeUser = await _userFacade.GetAsync(ActiveUserId);
+
+            if (activeUser == null)
+            {
+                throw new NullReferenceException("An error occured trying to leave project.");
+            }
+
+            ProjectAmountListModel projectAmountToDelete = activeUser.Projects.SingleOrDefault<ProjectAmountListModel>(u => u.UserId == activeUser.Id && u.ProjectId == projectListModel.Id)!;
+            
+            if (projectAmountToDelete != null)
+            {
+                activeUser.Projects.Remove(projectAmountToDelete);
+
+                await _projectAmountFacade.DeleteAsync(projectAmountToDelete.Id);
+
+                MessengerService.Send(new UserToProjectAdd());
+            }
+            else
+            { 
+                await _alertService.DisplayAsync("JoinUserIsNotInProject", "User is not in this project"); // fix alerts
+            }
+        }
+    }
+
+    public async void Receive(UserToProjectAdd message)
+    {
+
         await LoadDataAsync();
     }
+
     public async void Receive(ProjectEditMessage message)
     {
         await LoadDataAsync();
