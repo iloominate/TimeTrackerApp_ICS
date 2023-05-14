9d77a38022c85320b5ea1e8db4babe64eb9ff0c8
SyruSTR <dargonoed25@mail.ru>
2023-05-14 21:05:20 +0200

    Add Comments

--- TimeTracker.BL.Tests/ActivityFacadeTests.cs
+++ TimeTracker.BL.Tests/ActivityFacadeTests.cs
@@ -22,7 +22,9 @@ public sealed  class ActivityFacadeTests : FacadeTestsBase
     [Fact]
     public async Task GetById_NonExistent()
     {
+        // Arrange
         var activity = await _activityFacadeSUT.GetAsync(Guid.Parse("607c5e57-a7a9-4303-beab-9f6726af6b26"));
+        // Assert
         Assert.Null(activity);
     }
 
@@ -30,14 +32,17 @@ public sealed  class ActivityFacadeTests : FacadeTestsBase
     [Fact]
     public async Task AddNewActivity_Crossing()
     {
+        // Arrange
         var model = ActivityModelMapper.MapToDetailModel(ActivitySeeds.Generator);
 
+        // Assert
         _ = Assert.ThrowsAsync<DbUpdateException>(() => _activityFacadeSUT.SaveAsync(model));
     }
 
     [Fact]
     public async Task AddNewActivity_NotCrossing()
     {
+        // Arrange
         var model = ActivitySeeds.LevelDesign with
         {
             Id = Guid.Parse("836b3e93-fa20-4c93-a7ea-1ab4f59a32a8"),
@@ -45,6 +50,7 @@ public sealed  class ActivityFacadeTests : FacadeTestsBase
             End = DateTime.Parse("05/05/2023 11:00:00"),
         };
 
+        // Assert
         await _activityFacadeSUT.SaveAsync(ActivityModelMapper.MapToDetailModel(model));
         Assert.True(true);
     }
