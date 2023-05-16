58b06ed461ceb92147160c398c562b81007dac6e
murad <mikogazievm@mail.ru>
2023-05-16 12:43:30 +0200

    window size on launch is now constant 1920x1080

--- TimeTracker.App/App.xaml.cs
+++ TimeTracker.App/App.xaml.cs
@@ -8,4 +8,17 @@ public partial class App : Application
 
         MainPage = new AppShell();
     }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        const int newWidth = 1920;
+        const int newHeight = 1080;
+
+        window.Width = newWidth;
+        window.Height = newHeight;
+
+        return window;
+    }
 }
